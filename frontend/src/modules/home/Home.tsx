c5e1bd1696469bd6e045efc098223120232127c8
Sajjan Maharjan <sajjan.maharjan@krafters.dev>
2022-12-21 08:04:47 +0545

    Tailwind setup and configured

--- frontend/src/modules/home/Home.tsx
+++ frontend/src/modules/home/Home.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Create a Question?</div>
+    <div>
+        <div className='font-bold text-center text-2xl'>Create a Question?</div>
+    </div>
   )
 }
 
